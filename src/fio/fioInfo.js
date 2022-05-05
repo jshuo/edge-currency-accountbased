c33b7bb76ec6da60238d571ab9ab2e4e24ea7d05
jshuo <44391079+jshuo@users.noreply.github.com>
2022-05-05 21:49:10 +0800

    Update fioInfo.js

--- src/fio/fioInfo.js
+++ src/fio/fioInfo.js
@@ -5,28 +5,15 @@ import { type EdgeCurrencyInfo } from 'edge-core-js/types'
 
 import { FIO_REQUESTS_TYPES } from './fioConst'
 
-const defaultSettings: any = {
+const defaultSettings = {
   apiUrls: [
-    'https://fio.eu.eosamsterdam.net/v1/',
-    'https://fio.eosdac.io/v1/',
-    'https://fio.eosphere.io/v1/',
-    'https://fio.eosrio.io/v1/',
-    'https://fio.acherontrading.com/v1/',
-    'https://fio.eos.barcelona/v1/',
-    'https://api.fio.eosdetroit.io/v1/',
-    'https://api.fio.alohaeos.com/v1/',
-    'https://fio.greymass.com/v1/',
-    'https://fio.eosargentina.io/v1/',
-    'https://fio.cryptolions.io/v1/',
-    'https://api.fio.currencyhub.io/v1/',
-    'https://fio.eoscannon.io/v1/',
-    'https://fio.eosdublin.io/v1/',
-    'https://api.fiosweden.org/v1/'
+    'http://testnet.fioprotocol.io/v1/',
+    'https://api.testnet.fiosweden.org',
+    'https://fiotestnet.blockpane.com'
   ],
   historyNodeUrls: [
-    'https://fio.greymass.com/v1/',
-    'https://fio.greymass.com/v1/',
-    'https://fio.eosphere.io/v1/'
+    'https://fiotestnet.blockpane.com/v1/chain',
+    'https://fio-testnet.eosphere.io'
   ],
   fioRegApiUrl: 'https://reg.fioprotocol.io/public-api/',
   fioDomainRegUrl: 'https://reg.fioprotocol.io/domain/',
@@ -62,8 +49,8 @@ export const currencyInfo: EdgeCurrencyInfo = {
 
   defaultSettings,
 
-  addressExplorer: 'https://fio.bloks.io/key/%s',
-  transactionExplorer: 'https://fio.bloks.io/transaction/%s',
+  addressExplorer: 'https://fio-test.bloks.io/key/%s',
+  transactionExplorer: 'https://fio-test.bloks.io/transaction/%s',
 
   denominations: [
     // An array of Objects of the possible denominations for this currency
